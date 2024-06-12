e6cabef30ab656d4f9d7c3723bd573e5e43d9cfe
김우형 <hyeong7272@daum.net>
2024-06-12 16:21:56 +0900

    WebRTC, WebSocket, real-time

--- src/server.js
+++ src/server.js
@@ -21,10 +21,19 @@ const sockets = [];
 
 wss.on("connection", (socket) => {
     sockets.push(socket);
+    socket["nickname"] = "익명";
     console.log("Connected to Browser");
     socket.on("close", () => console.log("Disconnected from the browser"));
-    socket.on("message", (message) => {
-        sockets.forEach(aSocket => aSocket.send(message.toString()));
+    socket.on("message", (msg) => {
+        const message = JSON.parse(msg);
+        switch(message.type) {
+            case "new_message":
+                sockets.forEach(aSocket =>
+                    aSocket.send(`${socket.nickname}: ${message.payload}`));
+            case "nickname":
+                socket["nickname"] = message.payload;
+        }
+
     });
 });
 
