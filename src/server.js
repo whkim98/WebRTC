b7060fe8688a76276a629a8925fe55579b62901f
김우형 <hyeong7272@daum.net>
2024-06-13 10:00:32 +0900

    socket.io 설치 및 설정

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@
 import http from 'http';
 import WebSocket from "ws";
 import express from "express";
+import SocketIO from "socket.io";
 
 const app = express();
 
@@ -14,27 +15,35 @@ app.get("/*", (req, res) => res.redirect("/")); // 어떤 매핑으로 들어가
 const handleListen = () => console.log("Listening on http://localhost:3000");
 
 //http서버와 웹소켓 서버를 둘 다 같은 포트에서 사용하기 위함
-const server = http.createServer(app); //http서버가 필요한 이유: views, static files, home, redirection을 하기 위함
-const wss = new WebSocket.Server({ server });
+const httpServer = http.createServer(app); //http서버가 필요한 이유: views, static files, home, redirection을 하기 위함
+const wsServer = SocketIO(httpServer);
 
-const sockets = [];
+wsServer.on("connection", (socket) => {
+    console.log(socket);
+})
 
-wss.on("connection", (socket) => {
-    sockets.push(socket);
-    socket["nickname"] = "익명";
-    console.log("Connected to Browser");
-    socket.on("close", () => console.log("Disconnected from the browser"));
-    socket.on("message", (msg) => {
-        const message = JSON.parse(msg);
-        switch(message.type) {
-            case "new_message":
-                sockets.forEach(aSocket =>
-                    aSocket.send(`${socket.nickname}: ${message.payload}`));
-            case "nickname":
-                socket["nickname"] = message.payload;
-        }
-    });
-});
+//WebSocket만을 이용한 코드 주석 처리
+// const wss = new WebSocket.Server({ server });
 
-server.listen(3000, handleListen);
+// const sockets = [];
+//
+// wss.on("connection", (socket) => {
+//     sockets.push(socket);
+//     socket["nickname"] = "익명";
+//     console.log("Connected to Browser");
+//     socket.on("close", () => console.log("Disconnected from the browser"));
+//     socket.on("message", (msg) => {
+//         const message = JSON.parse(msg);
+//         switch(message.type) {
+//             case "new_message":
+//                 sockets.forEach(aSocket =>
+//                     aSocket.send(`${socket.nickname}: ${message.payload}`));
+//             case "nickname":
+//                 socket["nickname"] = message.payload;
+//         }
+//     });
+// });
 
+httpServer.listen(3000, handleListen);
+
+//SOCKET.IO는 WebSocket의 부가기능이 아닌 별도의 framework임
